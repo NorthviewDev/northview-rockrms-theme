1bd70eb71dee86cc884c5f8ffa31a9cb1837d754
Ben Goshow <ben@fishhook.us>
2020-02-06 10:57:41 -0500

    adding jsscrollpane

--- Themes/NorthviewChurch/Assets/Scripts/global.js
+++ Themes/NorthviewChurch/Assets/Scripts/global.js
@@ -1,6 +1,8 @@
 //Global Scripts
 $(function() { //document.ready
     
+	$('#location-search').jScrollPane();
+	
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
   let vh = window.innerHeight * 0.01;
   // Then we set the value in the --vh custom property to the root of the document
