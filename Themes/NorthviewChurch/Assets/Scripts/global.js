36820bf86a77255d4d3718557e0a1f0aa9601564
Ben Goshow <ben@fishhook.us>
2020-02-06 13:46:28 -0500

    Locations Map updates - moving to MapBox, adding JSScrollPane

--- Themes/NorthviewChurch/Assets/Scripts/global.js
+++ Themes/NorthviewChurch/Assets/Scripts/global.js
@@ -1,7 +1,5 @@
 //Global Scripts
 $(function() { //document.ready
-    
-	$('#location-search').jScrollPane();
 	
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
   let vh = window.innerHeight * 0.01;
