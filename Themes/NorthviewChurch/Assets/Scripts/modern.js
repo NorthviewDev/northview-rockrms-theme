6adf61ec4f470eac743499cf8db359b091e3f2b1
Ben Goshow <ben@fishhook.us>
2020-04-30 15:29:40 -0400

    media hub design

--- Themes/NorthviewChurch/Assets/Scripts/modern.js
+++ Themes/NorthviewChurch/Assets/Scripts/modern.js
@@ -1,5 +1,20 @@
 //Global Scripts
 $(function() { //document.ready
+
+  /*** Clickable Card layout ***/
+  /*** https://inclusive-components.design/cards/#theredundantclickevent ***/
+  const cards = document.querySelectorAll('.c-card,.masonry-grid__grid-item');  
+  Array.prototype.forEach.call(cards, card => {
+    card.style.cursor = 'pointer';
+    let down, up, link = card.querySelector('a');
+    card.onmousedown = () => down = +new Date();
+    card.onmouseup = () => {
+      up = +new Date();
+      if ((up - down) < 200) {
+        link.click();
+      }
+    }
+  });
 	
   // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
   let vh = window.innerHeight * 0.01;
