5548d2460670c54c01d162ac950657127ee8ba1d
Ben Goshow <ben@fishhook.us>
2021-04-14 15:35:14 -0400

    sync files from server

---
+++ Themes/NorthviewChurch/Layouts/Blank.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+using Rock;
+using Rock.Attribute;
+using Rock.CheckIn;
+using Rock.Model;
+
+namespace RockWeb.Themes.NorthviewChurch.Layouts
+{
+    partial class CustomThemePage : Rock.Web.UI.RockPage
+    {
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            
+            var bodyTag = this.Page.Master.FindControl( "bodyTag" ) as HtmlGenericControl;
+            if ( bodyTag != null )
+            {
+                bodyTag.AddCssClass( "no-header" );
+            }
+        }
+    }
+}
